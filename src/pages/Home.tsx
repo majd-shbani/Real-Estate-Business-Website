d1b8a427e74b395252ef4a2d3d4ce7994f414dc6
ammarbaroudi <122110280+ammarbaroudi@users.noreply.github.com>
2026-01-14 15:58:20 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@
 import SectionHeader from '../components/SectionHeader'
 const Home = () => {
     return (
-        <div className="container mx-auto px-4 bg-gray-50 dark:bg-[#141414] min-h-screen">
+        <div className="">
          
         </div>
     )
