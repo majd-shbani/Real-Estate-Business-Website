a11474f41041de8d30d61de4d37bf496ea06207d
majd-shbani <majdshbani9@gmail.com>
2026-01-15 19:17:11 +0300

    firebaseConfiq

---
+++ src/firebaseConfiq.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCZcUb5QFAyAVmS76sxL8kmv1A8Z3K2dfU",
+    authDomain: "real-estate-c6ea8.firebaseapp.com",
+    projectId: "real-estate-c6ea8",
+    storageBucket: "real-estate-c6ea8.firebasestorage.app",
+    messagingSenderId: "692737108107",
+    appId: "1:692737108107:web:52e734009124dac1380c8e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
