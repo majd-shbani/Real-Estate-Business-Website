717c8f23aca69ae55be492086bb501beeb39f6cc
Yara <lilysoufi22@gmail.com>
2026-01-17 14:10:46 +0200

    update footer and start journey

---
+++ src/interfaces/Footer/Footer.ts
@@ -0,0 +1,34 @@
+export interface EmailFooter {
+    emailIcon : string
+    placeholder : string 
+    telegramIcon : string
+}
+
+export interface Service {
+    service : string
+}
+export interface FooterService {
+    serviceTitle : string
+    services : Service[]
+}
+
+export interface SocialMedia {
+    icon : string
+}
+
+
+export interface FooterProps {
+    imageIcon : string
+    imageName : string
+    sendEmail : EmailFooter
+    footerService : FooterService[]
+    copyright : string
+    terms : string
+    socialMedia : SocialMedia[]
+  
+    
+}
+
+export interface Footer {
+    footerProps : FooterProps
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
+import Footer, { type FooterProps } from "../components/Footer/Footer"
+import StartJourney from "../components/StartJourney/StartJourney"
+import { footerProps } from "../data/Footer/Footer"
+import { journeyProps } from "../data/JourneyStart/StartJourney"
+
+
+
+
 
 
 const Home = () => {
     return (
         <div>
-    
+            <StartJourney journeyProps={journeyProps} />
+           <Footer footerProps={footerProps} />
+          
         </div>
     )
 }
