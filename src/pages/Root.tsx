85ca7f0b0158ec69fa90fe50336f6d89cd09c34d
majd-shbani <majdshbani9@gmail.com>
2026-01-19 02:08:38 +0300

    navigation Section

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,12 +1,20 @@
 import { Outlet } from "react-router-dom"
 import Nav from "../components/Nav/Nav"
+import StartJourney from "../components/StartJourney/StartJourney"
+
+import { journeyProps } from "../data/JourneyStart/StartJourney"
+import { footerProps } from "../data/Footer/Footer"
+import Footer from "../components/Footer/Footer"
+
 
 
 const Root = () => {
     return (
-        <div>
+        <div className={`text-white bg-gray-08 `}>
             <Nav/>
             <Outlet/>
+            <StartJourney journeyProps={journeyProps} />
+            <Footer footerProps={footerProps} />
         </div>
     )
 }
