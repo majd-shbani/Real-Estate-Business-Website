71d1042c2b82093a885dce8436a2cf1c31c11eb8
Mohammad-Alkreima <mohammadalkreima@gmail.com>
2026-01-19 00:44:25 +0400

    edit hero page-1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import Footer, { type FooterProps } from "../components/Footer/Footer"
-import HeroHome from "../components/HeroHome/HeroHome"
 import StartJourney from "../components/StartJourney/StartJourney"
 import { footerProps } from "../data/Footer/Footer"
 import { journeyProps } from "../data/JourneyStart/StartJourney"
