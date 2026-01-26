9cb03839845fc7bc997df6303dbd754663999860
maya-farha <maiaafarha@gmail.com>
2026-01-26 15:02:45 +0300

    Unblock Card

--- src/pages/Properties.tsx
+++ src/pages/Properties.tsx
@@ -3,12 +3,12 @@ import FeaturedSection from "../components/FeaturedSection/FeaturedSection"
 import HeroProperties from "../components/HeroProperties/HeroProperties"
 
 const Properties = () => {
-    return (
-        <div>
-            <HeroProperties />
-            <FeaturedSection />
-        </div>
-    )
+      return (
+            <div>
+                  <HeroProperties />
+                  <FeaturedSection />
+            </div>
+      )
 }
 
 export default Properties
