3ff368a4bb8257f8f6ffdb7d4cec137710d79f70
majd-shbani <majdshbani9@gmail.com>
2026-01-27 03:46:24 +0300

    edit

--- src/pages/PropertyDetails.tsx
+++ src/pages/PropertyDetails.tsx
@@ -1,12 +1,13 @@
 
 
 import FaqSection from "../components/FaqSection/FaqSection"
+import PricingSection from "../components/PricingSection/PricingSection"
 
 
 const PropertyDetails = () => {
     return (
         <div>
-            {/* <PricingSection title="Listing Price" price="1,250,000" /> */}
+            <PricingSection title="Listing Price" price="1,250,000" /> 
             <FaqSection />
         </div>
     )
