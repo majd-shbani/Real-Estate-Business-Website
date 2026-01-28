ad997ddabe0ed7157bd2d1fbcccce5561ddf2e41
M-Karakit <mohamad.karakit98@gmail.com>
2026-01-28 06:41:34 +0300

    complete CRUD operations on our team section using websocket(firebase) and redux toolkit, modify form input component and some sections.

--- src/interfaces/OurTeamSection/OurTeamSection.ts
+++ src/interfaces/OurTeamSection/OurTeamSection.ts
@@ -1,8 +1,10 @@
 export interface EmployeeCardProps {
     card: {
-        id: number;
+        id: string | number;
         image: string;
         name: string;
         position: string;
     }
+    onEdit?: (id: string | number) => void;
+    onDelete?: (id: string | number) => void;
 }
