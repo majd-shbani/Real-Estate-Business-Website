36ff1e68711a21d5156b188b9e1ef029ba190719
majd-shbani <majdshbani9@gmail.com>
2026-01-28 21:52:21 +0300

    edit pages Components

--- src/pages/PropertyDetails.tsx
+++ src/pages/PropertyDetails.tsx
@@ -1,13 +1,13 @@
-import InquireAboutSeaside from "../components/InquireAboutSeaside/InquireAboutSeaside"
+
 
 
 import FaqSection from "../components/FaqSection/FaqSection"
+import InquireAboutSeaside from "../components/InquireAboutSeaside/InquireAboutSeaside"
 import PricingSection from "../components/PricingSection/PricingSection"
 
 import PropertyDetailsHeader from "../components/PropertyDetailsHeader/PropertyDetailsHeader"
-import PropertyKeyFeatures from "../components/PropertyKeyFeatures/PropertyKeyFeatures"
+
 import { property } from "../data/PropertyDetailsHeader/PropertyDetailsHeader"
-import { propertyKeyData } from "../data/PropertyKeyFeatures/PropertyKeyFeatures"
 
 
 
@@ -18,8 +18,7 @@ const PropertyDetails = () => {
     return (
         <div>
             <PropertyDetailsHeader property={property}/>
-            <PropertyKeyFeatures card={propertyKeyData} />
-
+            <InquireAboutSeaside/>
             <PricingSection title="Listing Price" price="1,250,000" /> 
             <FaqSection />
         </div>
