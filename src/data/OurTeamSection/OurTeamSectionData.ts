ed6322b0f0f5dbf5d27eead019b44d8536e49c7d
M-Karakit <mohamad.karakit98@gmail.com>
2026-01-29 04:32:58 +0300

    fix paths for images, modify form input with the interface and employee form in dashboard.

--- src/data/OurTeamSection/OurTeamSectionData.ts
+++ src/data/OurTeamSection/OurTeamSectionData.ts
@@ -1,33 +1,6 @@
-export const EmployeeCardData = [
-    {
-        id: 1,
-        image: "assets/images/employee-1.webp",
-        name: "Max Mitchell",
-        position: "Founder",
-    },
-    {
-        id: 2,
-        image: "assets/images/employee-2.webp",
-        name: "Sarah Johnson",
-        position: "Chief Real Estate Officer",
-    },
-    {
-        id: 3,
-        image: "assets/images/employee-3.webp",
-        name: "David Brown",
-        position: "Head of Property Management",
-    },
-    {
-        id: 4,
-        image: "assets/images/employee-4.webp",
-        name: "Michael Turner",
-        position: "Legal Counsel",
-    },
-]
-
 export const EMPLOYEE_CARD_DEFAULTS = {
-    twitterIcon: "assets/icons/twitter-icon.svg",
-    sendIcon: "assets/icons/send-icon.svg",
+    twitterIcon: "/assets/icons/twitter-icon.svg",
+    sendIcon: "/assets/icons/send-icon.svg",
     placeholder: "Say Hello 👋"
 } as const;
 
