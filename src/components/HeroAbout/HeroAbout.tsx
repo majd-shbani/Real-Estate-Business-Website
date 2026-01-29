27b6e7720ba084fde81be88adcbf06eee0a14d27
Mohammad-Alkreima <mohammadalkreima@gmail.com>
2026-01-30 02:49:13 +0400

    edit some problems

--- src/components/HeroAbout/HeroAbout.tsx
+++ src/components/HeroAbout/HeroAbout.tsx
@@ -4,9 +4,9 @@ import StatisticeSection from "../StatisticeSection/StatisticeSection"
 
 const HeroAbout = ({img, alt}: HeroAboutProps) => {
     return (
-        <div className=" h-screen mt-[50px] xl:mt-[70px] 2xl:mt-25 flex flex-col-reverse xl:flex-row xl:items-center xl:justify-between pr-4 lg:pr-[7.8125vw] xl:pr-[5.55555vw] 2xl:pr-[8.4375vw]">
-            <Section className="mb-0 xl:pr-[60px]! 2xl:pr-20!" title="Our Journey" description="Our story is one of continuous growth and evolution. We started as a small team with big dreams, determined to create a real estate platform that transcended the ordinary. Over the years, we've expanded our reach, forged valuable partnerships, and gained the trust of countless clients.">
-                <StatisticeSection/>
+        <div className="pt-36 xl:pt-0 my-10 xl:my-[98px] 2xl:my-36 h-screen  flex flex-col-reverse justify-evenly xl:flex-row xl:items-center xl:justify-between gap-10 xl:gap-15 2xl:gap-[80px] pr-0  xl:pr-[5.55555vw] 2xl:pr-[8.4375vw] pt-0 my-[30px] xl:mt-0">
+            <Section className="xl:w-[59.3229vw]! mb-0! xl:pr-0!" title="Our Journey" description="Our story is one of continuous growth and evolution. We started as a small team with big dreams, determined to create a real estate platform that transcended the ordinary. Over the years, we've expanded our reach, forged valuable partnerships, and gained the trust of countless clients.">
+                <StatisticeSection className="[&>div]:py-[18px] [&>div]:xl:p-5 [&>div]:2xl:p-6 [&>div]:px-4"/>
             </Section>
             <div className="mx-auto mb-10 xl:mb-0 xl:mx-0 px-4 xl:px-0">
                 <img src={img} alt={alt} />
