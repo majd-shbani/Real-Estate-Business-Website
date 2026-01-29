509ebe42586ac0a73904fe490cfbc82ece05c9d7
Yara <lilysoufi22@gmail.com>
2026-01-29 13:43:52 +0200

    fix images

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,10 +1,14 @@
+import PropertyKeyFeatures from "../components/PropertyKeyFeatures/PropertyKeyFeatures"
+import ServiceInvestments from "../components/ServiceInvestments/ServiceInvestments"
+import { propertyKeyData } from "../data/PropertyKeyFeatures/PropertyKeyFeatures"
 
 
 const Services = () => {
     return (
         <div>
 
-       
+           <ServiceInvestments />
+           <PropertyKeyFeatures card={propertyKeyData} />
         </div>
     )
 }
