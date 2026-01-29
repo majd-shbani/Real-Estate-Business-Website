ea005b4c7cca380df47430ab501e7b52a0559db7
Mohammad-Alkreima <mohammadalkreima@gmail.com>
2026-01-29 09:44:25 +0400

    finalize megre cleanup for PropertyDetails

--- src/pages/PropertyDetails.tsx
+++ src/pages/PropertyDetails.tsx
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 9dd58fe72d14ab31b10921a44fbf8adf12b4e6bb
 
 import FaqSection from "../components/FaqSection/FaqSection"
 import InquireAboutSeaside from "../components/InquireAboutSeaside/InquireAboutSeaside"
 import PricingSection from "../components/PricingSection/PricingSection"
-import PropertyKeyFeatures from "../components/PropertyKeyFeatures/PropertyKeyFeatures"
 
 import PropertyDetailsHeader from "../components/PropertyDetailsHeader/PropertyDetailsHeader"
 
@@ -21,12 +17,8 @@ import { property } from "../data/PropertyDetailsHeader/PropertyDetailsHeader"
 const PropertyDetails = () => {
     return (
         <div>
-<<<<<<< HEAD
-            <PropertyKeyFeatures />
-=======
             <PropertyDetailsHeader property={property}/>
             <InquireAboutSeaside/>
->>>>>>> 9dd58fe72d14ab31b10921a44fbf8adf12b4e6bb
             <PricingSection title="Listing Price" price="1,250,000" /> 
             <FaqSection />
         </div>
