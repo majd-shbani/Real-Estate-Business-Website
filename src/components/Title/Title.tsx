cb4722f9507c211b861272730bdec8e5fc1bb279
Mohammad-Alkreima <mohammadalkreima@gmail.com>
2026-02-02 19:16:25 +0400

    edit problems in herohome, heroabout, connect, textcontainer

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -4,10 +4,10 @@ import type { TitleProps } from "../../interfaces/HeroHome/HeroHome"
 const Title = ({ title, description}: TitleProps) => {
     return (
         <div>
-            <h1 className="text-[28px] xl:text-[40px] 2xl:text-6xl 2xl:mb-6 font-semibold leading-[1.2] mb-4">
+            <h1 className="my-4 xl:mt-0 xl:mb-5 2xl:mb-6  text-[28px] xl:text-[46px] 2xl:text-6xl font-semibold leading-[120%]">
                 {title}
             </h1>
-            <p className="text-gray-60 text-sm 2xl:text-lg font-medium leading-normal mb-10 2xl:mb-15">{description}</p>
+            <p className="mb-10 xl:mb-[50px] 2xl:mb-15 text-gray-60 text-[16px] 2xl:text-lg font-medium leading-[150%]">{description}</p>
         </div>
     )
 }
