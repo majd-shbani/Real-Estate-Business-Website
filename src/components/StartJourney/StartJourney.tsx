d43c7cc1334f32e2331b62d70cf1c91e2a6de418
Yara <lilysoufi22@gmail.com>
2026-02-04 23:57:47 +0200

    light mode

--- src/components/StartJourney/StartJourney.tsx
+++ src/components/StartJourney/StartJourney.tsx
@@ -4,7 +4,7 @@ import Button from "../Button/Button"
 const StartJourney = (journeyProps : StartJourneyProps) => {
   return (
 
-      <div className="w-full px-4 md:px-[5.208333vw] xl:px-[5.55555vw] 2xl:px-[8.4375vw]  py-12.5 md:py-20  lg:py-15  2xl:py-25 relative  overflow-hidden  h-91.75 lg:h-64.75 2xl:h-91.75 border border-Royal-Indigo dark:border-gray-15 leading-[150%]">
+      <div className="w-full px-4 md:px-[5.208333vw] xl:px-[5.55555vw] 2xl:px-[8.4375vw]  py-12.5 md:py-20  lg:py-15  2xl:py-25 relative  overflow-hidden  h-91.75 lg:h-64.75 2xl:h-91.75 border border-Silver dark:border-gray-15 leading-[150%]">
 
        <img className="invert dark:invert-0 absolute  rotate-180 top-0 right-45 sm:right-60 sm:top-20 sm:rotate-0 lg:top-0  md:left-0 md:rotate-0 md:top-15 xl:-top-13  xl:left-0 xl:rotate-0 2xl:top-14 " src={journeyProps.journeyProps.leftImage}></img>
        <img className="invert dark:invert-0 absolute sm:-bottom-10 sm:-right-10 bottom-0 right-0 md:bottom-0 md:right-0" src={journeyProps.journeyProps.rightImage}></img>
