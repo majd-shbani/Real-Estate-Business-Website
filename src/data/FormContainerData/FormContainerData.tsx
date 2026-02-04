af05c06bc44ddbb11a966263f3e8cf6c39f60187
M-Karakit <mohamad.karakit98@gmail.com>
2026-02-04 08:04:01 +0300

    modify light/dark for my sections + add the middle border to Values section with light/dark theme.

--- src/data/FormContainerData/FormContainerData.tsx
+++ src/data/FormContainerData/FormContainerData.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const DEFAULT_FORM_DATA = {
     terms: { 
         text: (
             <>
-                I agree with <span className="underline hover:text-purple-60 decoration-gray-30 cursor-pointer">Terms of Use</span> and <span className="underline hover:text-purple-60 decoration-gray-30 cursor-pointer">Privacy Policy</span>
+                I agree with <Link to={'#'} className="underline hover:text-Indigo hover:dark:text-purple-60 decoration-gray-30 hover:decoration-Indigo hover:dark:decoration-purple-60 cursor-pointer">Terms of Use</Link> and <Link to={'#'} className="underline hover:text-Indigo hover:dark:text-purple-60 decoration-gray-30 hover:decoration-Indigo hover:dark:decoration-purple-60  cursor-pointer">Privacy Policy</Link>
             </>
         )
     },
